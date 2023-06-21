6f8d351fb13ae21a0d81f2b7dc98ff6341714c12
qqsun <qqsun@datav.com>
2023-06-21 16:04:13 +0800

    second commit

---
+++ docs/.vuepress/dist/public/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    themeConfig: {
+        home: '/index.html',
+        nav: [
+            {
+                text: 'Languages',
+                ariaLabel: 'Language Menu',
+                items: [
+                    { text: 'Chinese', link: '/language/chinese/' },
+                    { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            }
+        ]
+    }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,9 @@
 '# Hello VuePress' 
+---
+title: My Document
+date: 2023-06-21
+author: John Doe
+tags:
+  - VuePress
+  - Documentation
+---
